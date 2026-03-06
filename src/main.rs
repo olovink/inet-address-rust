1ea88792fb3a2a0768a393a8d7bf668ac286e948
olovink <69630128+olovink@users.noreply.github.com>
2026-03-07 04:15:06 +0700

    its only for ipv4

--- src/main.rs
+++ src/main.rs
@@ -4,22 +4,21 @@ use std::net::Ipv4Addr;
 struct InetAddress {
     address: [u8; 4],
     port: u16,
-    version: u8
 }
 
 impl InetAddress {
-    pub const fn new(address: [u8; 4], port: u16, version: u8) -> InetAddress {
-        InetAddress {address, port, version}
+    pub const fn new(address: [u8; 4], port: u16) -> InetAddress {
+        InetAddress {address, port}
     }
 
     pub fn get_string_address(&self) -> String {
         format!("{}.{}.{}.{}", self.address[0], self.address[1], self.address[2], self.address[3])
     }
 
-    pub const LOCALHOST: Self = InetAddress::new([127, 0, 0, 1], 0, 4);
+    pub const LOCALHOST: Self = InetAddress::new([127, 0, 0, 1], 0);
 
     pub fn equals(&self, other: &InetAddress) -> bool {
-        self.get_string_address() == other.get_string_address() && self.port == other.port && self.version == other.version
+        self.get_string_address() == other.get_string_address() && self.port == other.port
     }
 
     pub fn get_as_ipv4addr(&self) -> Ipv4Addr {
@@ -31,5 +30,5 @@ fn main() {
     let inet_address = InetAddress::LOCALHOST;
 
     println!("{:?}", inet_address);
-    println!("{}", InetAddress::new([127, 0, 0, 1], 0, 4).get_string_address());
+    println!("{}", InetAddress::new([127, 0, 0, 1], 0).get_string_address());
 }
