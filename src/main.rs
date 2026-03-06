fb3ff83da3ce8f38e619008760181256fe15957d
olovink <69630128+olovink@users.noreply.github.com>
2026-03-07 04:11:00 +0700

    upload main.ts

---
+++ src/main.rs
@@ -0,0 +1,29 @@
+#[derive(Copy, Clone, PartialEq, Eq, Debug)]
+struct InetAddress {
+    address: [u8; 4],
+    port: u16,
+    version: u8
+}
+
+impl InetAddress {
+    pub const fn new(address: [u8; 4], port: u16, version: u8) -> InetAddress {
+        InetAddress {address, port, version}
+    }
+
+    pub fn get_string_address(&self) -> String {
+        format!("{}.{}.{}.{}", self.address[0], self.address[1], self.address[2], self.address[3])
+    }
+
+    pub const LOCALHOST: Self = InetAddress::new([127, 0, 0, 1], 0, 4);
+
+    pub fn equals(&self, other: &InetAddress) -> bool {
+        self.get_string_address() == other.get_string_address() && self.port == other.port && self.version == other.version
+    }
+}
+
+fn main() {
+    let inet_address = InetAddress::LOCALHOST;
+
+    println!("{:?}", inet_address);
+    println!("{}", InetAddress::new([127, 0, 0, 1], 0, 4).get_string_address());
+}
